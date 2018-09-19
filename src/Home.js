62a9d048bc4ce1d9939749c9bc1d5203a9766d50
Ben T <benjamin.tellier@feed.xyz>
2018-09-19 12:21:33 +0100

    Removed "Sign out" button

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
       <div>
         <h1>Hello, <br />Welcome <br />to Feed</h1>       
        <button><Link to="/FormIn" className="active">Sign In</Link></button>
-       <button><Link to="/FormOut" className="active">Sign Out</Link></button>
+       {/*<button><Link to="/FormOut" className="active">Sign Out</Link></button>*/}
       </div>
     );
   }
