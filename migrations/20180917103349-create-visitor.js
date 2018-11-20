c1e05457e4683f8ce430859b6ea2307d85905481
Ben T <benjamin.tellier@feed.xyz>
2018-11-20 14:09:20 +0000

    udpates

--- migrations/20180917103349-create-visitor.js
+++ migrations/20180917103349-create-visitor.js
@@ -1,31 +1,31 @@
-'use strict';
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Visitors', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      visitorName: {
-        type: Sequelize.TEXT
-      },
-      visitorCompany: {
-        type: Sequelize.TEXT
-      },
-      visitedName: {
-        type: Sequelize.TEXT
-      },
-      reasonSelection: {
-        type: Sequelize.TEXT
-      },
-      date: {
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Visitors');
-  }
-};
+// 'use strict';
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//     return queryInterface.createTable('Visitors', {
+//       id: {
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         type: Sequelize.INTEGER
+//       },
+//       visitorName: {
+//         type: Sequelize.TEXT
+//       },
+//       visitorCompany: {
+//         type: Sequelize.TEXT
+//       },
+//       visitedName: {
+//         type: Sequelize.TEXT
+//       },
+//       reasonSelection: {
+//         type: Sequelize.TEXT
+//       },
+//       date: {
+//         type: Sequelize.DATE
+//       }
+//     });
+//   },
+//   down: (queryInterface, Sequelize) => {
+//     return queryInterface.dropTable('Visitors');
+//   }
+// };
--- models/visitor.js
+++ models/visitor.js
@@ -1,14 +1,14 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const Visitor = sequelize.define('Visitor', {
-    visitorName: DataTypes.TEXT,
-    visitorCompany: DataTypes.TEXT,
-    visitedName: DataTypes.TEXT,
-    reasonSelection: DataTypes.TEXT,
-    date: DataTypes.DATETIME
-  }, {});
-  Visitor.associate = function(models) {
-    // associations can be defined here
-  };
-  return Visitor;
-};
+// 'use strict';
+// module.exports = (sequelize, DataTypes) => {
+//   const Visitor = sequelize.define('Visitor', {
+//     visitorName: DataTypes.TEXT,
+//     visitorCompany: DataTypes.TEXT,
+//     visitedName: DataTypes.TEXT,
+//     reasonSelection: DataTypes.TEXT,
+//     date: DataTypes.DATETIME
+//   }, {});
+//   Visitor.associate = function(models) {
+//     // associations can be defined here
+//   };
+//   return Visitor;
+// };
